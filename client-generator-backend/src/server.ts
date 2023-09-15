30d6a1264e5babd5a87f25276952c670dc7954e9
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-09-14 22:55:11 -0300

    feat: add more headers to cors on backend

--- client-generator-backend/src/server.ts
+++ client-generator-backend/src/server.ts
@@ -15,10 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", 
-             "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers", 
-             "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 

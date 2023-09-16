2b7eb04b42a2b3190bf8dd0d4c49436130fbcd16
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-09-15 22:27:43 -0300

    feat: remove cors

--- client-generator-backend/src/server.ts
+++ client-generator-backend/src/server.ts
@@ -15,17 +15,17 @@ const route = Router();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({
-  origin: "http://localhost:4200",
-  optionsSuccessStatus: 200
-}));
+// app.use(cors({
+//   origin: "http://localhost:4200",
+//   optionsSuccessStatus: 200
+// }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 route.get("/", (req: Request, res: Response) => {
   res.send("Express backend to generate client code from AsyncAPI specification");
